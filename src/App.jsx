183752cdb8ea0aecb12876c5bf6ffc509f6a0bee
Vanessa Blaho <vanessadblaho@gmail.com>
2023-11-05 16:37:03 +0100

    useRefs to save inputs

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,54 @@
+import { useState } from "react";
 import NewProject from "./Components/NewProject";
 import NoProjectSelected from "./Components/NoProjectSelected";
 import ProjectsSidebar from "./Components/ProjectsSidebar";
 
 function App() {
+  const [projectsState, setProjectsState] = useState({
+    selectedProjectId:undefined,
+    // undefined: neither adding a new project or have a new project selected
+    projects: []
+  });
+  function handleStartAddProject(){
+    setProjectsState(prevState=>{
+      return{
+        // null means that we're adding a new project
+        ...prevState,
+        selectedProjectId:null,
+
+      };
+    });
+  }
+  function handleAddProject (projectData){
+    setProjectsState (prevState => {
+      const newProject = {
+        ...projectData,
+        id: Math.random()
+      };
+
+      return {
+        ...prevState,
+        projects: [...prevState.projects, newProject]
+      };
+
+    })
+  }
+
+  console.log(projectsState);
+
+  let content;
+
+  if(projectsState.selectedProjectId === null){
+    content = <NewProject onAdd={handleAddProject}/>;
+  } else if(projectsState.selectedProjectId === undefined){
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject}/>;
+  }
   return (
     <>
     <main className= "h-screen my-8 flex gap-8">
-      <ProjectsSidebar/>
-      <NoProjectSelected/>
+      <ProjectsSidebar onStartAddProject={handleStartAddProject}/>
+     
+      {content}
       </main>
     </>
   );
--- src/Components/Input.jsx
+++ src/Components/Input.jsx
@@ -1,14 +1,23 @@
-
+import { forwardRef } from "react"
 /// reusable label component
 //label: the text that will appear as a label for the input field, ...props will pass any other props from a parent component
-export default function Input({label,textarea, ...props}){
+const Input = forwardRef (function Input({label,textarea, ...props }, ref){
+    
     const classes ="w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600"
 
-    return <p className="flex fle-col gap-1 my-4">
+    return (
+        <p className="flex fle-col gap-1 my-4">
         {/* //text of the label goes here */}
         <label className="text-sm font-bold uppercase text-stone-500">{label}</label>
         {/* if there is a textarea use the props if there is not a textare render an input */}
-            {textarea ? <textarea className={classes}{...props}/> :<input className={classes}/>}
+            {textarea ? (
+            <textarea ref={ref} className={classes}{...props}/>
+            ) :(
+            <input ref ={ref} className={classes} {...props}/>
+        )}
         
-    </p>
-}
+        </p>
+    );
+});
+
+export default Input;
