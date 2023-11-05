ac6dba7efef7f24c0c47ff4e11fa82d981bfe79b
Vanessa Blaho <vanessadblaho@gmail.com>
2023-11-05 14:40:48 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import NewProject from "./Components/NewProject";
+import NoProjectSelected from "./Components/NoProjectSelected";
+import ProjectsSidebar from "./Components/ProjectsSidebar";
+
+function App() {
+  return (
+    <>
+    <main className= "h-screen my-8 flex gap-8">
+      <ProjectsSidebar/>
+      <NoProjectSelected/>
+      </main>
+    </>
+  );
+}
+
+export default App;
